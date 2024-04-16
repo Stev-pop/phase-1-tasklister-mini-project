71b083785e323d331d4f3b5d7fb0b58ad4658c83
Stephen Onyango <onyangosteve01@gmail.com>
2024-04-17 02:54:47 +0300

    adds assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  const anotherTaskForm = document.querySelector('form');
+
+  //Event listener for submit button
+  anotherTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const anotherTask = e.target.querySelector('#new-task-description').value
+    buildToDo(anotherTask);
+
+    anotherTaskForm.reset();
+     })
+    });
+
+  function buildToDo(anotherTask){
+
+    const listItem = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    deleteButton.textContent = 'delete';
+
+    listItem.textContent = anotherTask;
+
+    listItem.appendChild(deleteButton);
+
+    document.querySelector('#tasks').appendChild(listItem);
+  
+  
+    //fAdd click event
+    deleteButton.addEventListener('click', handleDelete);
+  }
+
+  function handleDelete(e){
+    e.target.parentNode.remove();
+  }
